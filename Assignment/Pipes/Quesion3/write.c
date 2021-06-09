91bfd43c65a60a4d12238ba282e3197100b71a90
MohammedOwez1 <84498541+MohammedOwez1@users.noreply.github.com>
2021-06-09 10:00:09 +0530

    Add files via upload

---
+++ Assignment/Pipes/Quesion3/write.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	int fd;
+
+	char * msg = "/tmp/msg";
+	mkfifo(msg, 0666);
+
+	char arr1[80], arr2[80];
+	while (1)
+	{
+		fd = open(msg, O_WRONLY| O_CREAT);
+		fgets(arr2, 80, stdin);
+		write(fd, arr2, strlen(arr2)+1);
+		close(fd);
+		fd = open(msg, O_RDONLY|O_CREAT);
+		read(fd, arr1, sizeof(arr1));
+		printf("User2: %s\n", arr1);
+		close(fd);
+	}
+	return 0;
+}
